e7215410f47874f7a8f84deb7eb73c410a6f561c
Rosotics KH <eweissm1@asu.edu>
2023-06-05 12:46:55 -0700

    New Commit

--- main.py
+++ main.py
@@ -103,24 +103,58 @@ def buttonCommand_updateAutoOnOff(): # Toggles between manual and automated cont
         ser.write(bytes('GE', 'UTF-8'))  # Automated Signal
         print(bytes('GE', 'UTF-8'))
 def buttonCommand_RotateCW(): # manual control for rotating platform clockwise
+    global RotateDirectionState
+    RotateDirectionState = 1
+    RotateDirection.set("Clockwise")
     ser.write(bytes('ZE', 'UTF-8'))   # Rotate platform clockwise signal
     print(bytes('ZE', 'UTF-8'))
 
-def buttonCommand_RotateCCW(): # manual control for rotating platform clockwise
+def buttonCommand_RotateCCW(): # manual control for rotating platform counterclockwise
+    global RotateDirectionState
+    RotateDirectionState = 0
+    RotateDirection.set("Counter-Clockwise")
     ser.write(bytes('XE', 'UTF-8'))   # Rotate platform clockwise signal
     print(bytes('XE', 'UTF-8'))
 
 def buttonCommand_Rotate(): # manual control for rotating platform clockwise
     global TargetAngle
     TargetAngle = TargetAngleEntry.get()
-    TargetAngle = float(TargetAngle)/0.0177
+
     ser.write(bytes('C'+str(int(TargetAngle))+'E', 'UTF-8'))
     print(bytes('C'+str(int(TargetAngle))+'E', 'UTF-8'))
 
+def buttonCommand_updateAutoRPOnOff(): # Toggles between manual and automated control of rotating platform speed
+    global Automated_Controls_stateRP
+
+    # if we are in automated controls --> set to manual--> else set controls to automatic
+    if Automated_Controls_stateRP == 1:
+        Automated_Controls_stateRP = 0
+        AutoRPLabel.set("Automated Rotating Platform Controls: Off ")
+        ser.write(bytes('OE', 'UTF-8'))  # Manual Signal
+        print(bytes('OE', 'UTF-8'))
+    else:
+        Automated_Controls_stateRP = 1
+        AutoRPLabel.set("Automated Rotating Platform Controls: On ")
+        ser.write(bytes('PE', 'UTF-8'))  # Automated Signal
+        print(bytes('PE', 'UTF-8'))
+
+def buttonCommand_updateTargetAngleSpeed(): #Reads the txt entry and sends to serial message arduino to update target speed
+    global TargetAngleSpeed
+    TargetAngleSpeed = TargetAngleSpeedEntry.get()
+
+    ser.write(bytes('I'+str(int(TargetAngleSpeed))+'E', 'UTF-8'))
+    print(bytes('I'+str(int(TargetAngleSpeed))+'E', 'UTF-8'))
 
 # declare the automated controls to default at 0 (manual controls)
 Automated_Controls_state = 0
 Automated_Controls_stateWF = 0
+Automated_Controls_stateRP = 0
+
+# Actuator default selection is both
+ActuatorSelection_state = 2
+
+# Platform Rotation Direction default is counterclockwise
+RotateDirectionState = 0
 
 # Set up Serial Communication with Arduino------------------------------------------------------------------------------
 ser = serial.Serial('com3', 9600,writeTimeout=1)  # create Serial Object
@@ -129,7 +163,7 @@ time.sleep(3)  # delay 3 seconds to allow serial com to get established
 
 # Build GUI-------------------------------------------------------------------------------------------------------------
 tkTop = tkinter.Tk()  # Create GUI Box
-tkTop.geometry('1200x800')  # size of GUI
+tkTop.geometry('1200x1080')  # size of GUI
 tkTop.title("Test Stand Controller")  # title in top left of window
 
 Title = tkinter.Label(tkTop,text='Test Stand Controls', font=("Courier", 14, 'bold')).grid(row=0, column=0, rowspan=1, columnspan=2)  # Title on top middle of screen
@@ -184,6 +218,7 @@ button_both_state.pack(side='top', ipadx=10, padx=10, pady=40)
 
 ActuatorSelectionLabel = tkinter.IntVar()
 ActuatorSelection = tkinter.Label(master=LeftButtonsFrame, textvariable=ActuatorSelectionLabel)
+ActuatorSelectionLabel.set("Both")
 ActuatorSelection.pack()
 
 button_up_state = tkinter.Button(RightButtonsFrame,
@@ -229,6 +264,7 @@ button_Automated_on_off.pack(side='top', ipadx=0, padx=0, pady=20)
 
 varLabel = tkinter.IntVar()
 tkLabel = tkinter.Label(master=AutoFrame, textvariable=varLabel, bg="gray")
+varLabel.set("Automated Controls: Off")
 tkLabel.pack()
 
 TargetHeightLable = tkinter.Label(master=AutoFrame, text='Enter Target Height: ', font=("Courier", 12), bg="gray").pack(
@@ -308,6 +344,7 @@ button_AutoWF_OnOff.grid(row=1, column=0, columnspan=3, padx=10)
 
 varLabel1 = tkinter.IntVar()
 AutoWFLabel = tkinter.Label(master=AutoFrameWF, textvariable=varLabel1, bg="gray").grid(row=2, column=1)
+varLabel1.set("Automated Wire Feed Controls: Off")
 
 AutoFrameWF.grid(row=2,column=1, pady=20, sticky='N')
 
@@ -329,8 +366,87 @@ button_UpdateTarget.grid(row=3, column=2, padx=10)
 
 #Fill in rotating platform manual control
 ManualFrameRP = tkinter.Frame(master=tkTop, height=200, width=600)
-ManualLabelRP = tkinter.Label(master=ManualFrameRP, text='Manual Platform Controls', font=("Courier", 12)).grid()
+ManualLabelRP = tkinter.Label(master=ManualFrameRP, text='Manual Platform Controls', font=("Courier", 12, 'bold')).grid(row=0, column=0, columnspan=6)
+button_Clockwise = tkinter.Button(ManualFrameRP,
+                                  text="Clockwise",
+                                  command=buttonCommand_RotateCW,
+                                  height=2,
+                                  fg="black",
+                                  width=15,
+                                  bd=5,
+                                  activebackground='green')
+button_CounterClockwise = tkinter.Button(ManualFrameRP,
+                                  text="Counter-Clockwise",
+                                  command=buttonCommand_RotateCCW,
+                                  height=2,
+                                  fg="black",
+                                  width=15,
+                                  bd=5,
+                                  activebackground='green')
+button_Clockwise.grid(row=1, column=0, columnspan=3, padx=10, pady=20)
+button_CounterClockwise.grid(row=1, column=3, columnspan=3, padx=10, pady=20)
 
+RotateDirection = tkinter.IntVar()
+RotateDirectionLabel = tkinter.Label(master=ManualFrameRP, textvariable=RotateDirection, font=("Courier", 12)).grid(row=2, column=0, columnspan=6, padx=10, ipadx=20, pady=5)
+RotateDirection.set("Counter-Clockwise")
+
+TargetAngleLabel = tkinter.Label(master=ManualFrameRP, text="Rotate [deg]:", font=("Courier", 12)).grid(row=3, column=0, columnspan=2, padx=10, pady=5)
+TargetAngleEntry = tkinter.Entry(ManualFrameRP)
+TargetAngleEntry.grid(row=3, column=2, columnspan=2, padx=10, pady=5)
+
+button_TargetAngleUpdate = tkinter.Button(master=ManualFrameRP,
+                                          text="Rotate",
+                                          command=buttonCommand_Rotate,
+                                          height=2,
+                                          fg="black",
+                                          width=15,
+                                          bd=5,
+                                          activebackground='green')
+button_TargetAngleUpdate.grid(row=3, column=5, columnspan=2, padx=10, pady=5)
+
+ManualFrameRP.grid(row=3, column=0, pady=20, sticky="N")
+
+# Fill in automatic platform control frame
+AutoFrameRP = tkinter.Frame(master=tkTop, height=200, width=600, bg='gray')
+AutoFrameRPLabel = tkinter.Label(master=AutoFrameRP,
+                           text='Automatic Rotating Platform Controls',
+                           font=("Courier", 12, 'bold'),
+                           bg="gray").grid(row=0, column=0, columnspan=3, pady=20)  # Automatic rotating platform controls label
+
+button_AutoRP_OnOff = tkinter.Button(AutoFrameRP,
+                                     text="Automatic Rotating Platform Speed On/Off",
+                                     command=buttonCommand_updateAutoRPOnOff,
+                                     height=2,
+                                     fg="black",
+                                     width=40,
+                                     bd=5,
+                                     activebackground='green'
+                                     )
+button_AutoRP_OnOff.grid(row=1, column=0, columnspan=3, padx=10)
+
+AutoRPLabel = tkinter.IntVar()
+AutoRP = tkinter.Label(master=AutoFrameRP, textvariable=AutoRPLabel, bg="gray").grid(row=2, column=1)
+AutoRPLabel.set("Automatic Rotating Platform Controls: Off")
+
+AutoFrameRP.grid(row=2,column=1, pady=20, sticky='N')
+
+
+TargetAngleSpeedLabel = tkinter.Label(master=AutoFrameRP, text='Enter Target Angle Speed [deg/s]: ', font=("Courier", 12), bg="gray").grid(row=3,column=0, padx=10)
+TargetAngleSpeedEntry = tkinter.Entry(AutoFrameRP)
+TargetAngleSpeedEntry.grid(row=3, column=1, padx=10)
+
+button_UpdateTargetAngleSpeed = tkinter.Button(AutoFrameRP,
+                                     text="Update",
+                                     command=buttonCommand_updateTargetAngleSpeed,
+                                     height=2,
+                                     fg="black",
+                                     width=15,
+                                     bd=5,
+                                     activebackground='green'
+                                     )
+button_UpdateTargetAngleSpeed.grid(row=3, column=2, padx=10)
+
+AutoFrameRP.grid(row=3, column=1)
 
 tkinter.mainloop() # run loop watching for gui interactions
 
